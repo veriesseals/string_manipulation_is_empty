ef5983d9a49bedf8f4d1e936db3ab9f1b992c792
Veries Seals <veries@me.com>
2026-01-03 09:33:18 -0600

    Created two strings, one with content and one empty, Boolean variables to check  to see if the results of isEmpty() method was true or false, Printed the results

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        // isEmpty() method example
+        // Creates two strings, one with content and one empty
+        String testString1 = "This is a test";
+        String testString2 = "";
+
+
+        // Boolean variables to check  to see if the results of isEmpty() method is true or false
+    	boolean stringEmpty1 = testString1.isEmpty(); // false
+    	boolean stringEmpty2 = testString2.isEmpty();  // true
+
+
+        // Print the results
+ 
+        System.out.println("testString1.isEmpty() : " + stringEmpty1 + " There for results of isEmpty() is: " + testString1.isEmpty());
+
+        System.out.println("testString2.isEmpty() : " + stringEmpty2 + " There for results of isEmpty() is: " + testString2.isEmpty());
+        
+    }
+}
